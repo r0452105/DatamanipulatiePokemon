66ced3e694d73fa5378f00e38e746a56edbeffd6
Bryant <bryantsuiskensmast@gmail.com>
2020-05-04 13:31:35 +0200

    Initial Build
    Initial build with implementation comments.

---
+++ PokemonWPF/PokemonDAL/DatabaseOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonDAL
+{
+    public static class DatabaseOperations
+    {
+
+        public static Trainer SelectTrainer(int trainerId)
+        {
+
+            using (DB_r0739290Entities entities = new DB_r0739290Entities()) 
+            {
+                var query = entities.Trainer
+                            .Include("PlayerInventory")
+                            .Include("Pokemon")
+                            .Include("PokemonGroup")
+                            .Where(x => x.Id == trainerId);
+                return query.SingleOrDefault();
+
+            }
+
+                
+        }
+    }
+}
---
+++ PokemonWPF/PokemonWPF/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PokemonDAL;
+
+namespace PokemonWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> menuElements = new List<string>();
+        public Trainer trainer = DatabaseOperations.SelectTrainer(2);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            menuElements.Add("Pokedex");
+            menuElements.Add("Party");
+            menuElements.Add("Inventory");
+            menuElements.Add(trainer.TrainerName);
+            menuElements.Add("Exit");
+
+
+
+            lbMenu.ItemsSource = menuElements;
+        }
+
+        private void lbMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (lbMenu.SelectedIndex)
+            {
+                case 0: txbInfo.Text = "A encyclopedia of all seen pokemon";
+                    break;
+
+                case 1:
+                    txbInfo.Text = "Access your party screen";
+                    break;
+                case 2:
+                    txbInfo.Text = $"Show inventory of {trainer.TrainerName}";
+                    break;
+                case 3:
+                    txbInfo.Text = $"Show the trainercard of {trainer.TrainerName}";
+                    break;
+                default:  txbInfo.Text = "Close the program";
+                    break;
+            }
+        }
+
+        private void lbMenu_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            switch (lbMenu.SelectedIndex)
+            {
+                case 0:
+                    this.Visibility = Visibility.Hidden;
+                    //MAAK HIER DE CODE VOOR DE POKEDEX AAN TE ROEPEN MET SHOW DIALOG, EN PASS DE TRAINER VARIABLE ER NAAR TOE
+
+
+
+                    this.Visibility = Visibility.Visible;
+                    break;
+                case 1:
+                    this.Visibility = Visibility.Hidden;
+                    //MAAK HIER DE CODE VOOR DE PARTY AAN TE ROEPEN MET SHOW DIALOG, EN PASS DE TRAINER VARIABLE ER NAAR TOE
+
+
+
+                    this.Visibility = Visibility.Visible;
+                    break;
+                case 2:
+                    this.Visibility = Visibility.Hidden;
+                    //MAAK HIER DE CODE VOOR DE INVENTORY  TE ROEPEN MET SHOW DIALOG, EN PASS DE TRAINER VARIABLE ER NAAR TOE
+
+
+
+                    this.Visibility = Visibility.Visible;
+                    break;
+                case 3:
+                    this.Visibility = Visibility.Hidden;
+                    //MAAK HIER DE CODE VOOR DE TRAINER CARRD AAN TE ROEPEN MET SHOW DIALOG, EN PASS DE TRAINER VARIABLE ER NAAR TOE
+
+                    this.Visibility = Visibility.Visible;
+                    break;
+                default: Close();
+
+                    break;
+            }
+        }
+    }
+}
