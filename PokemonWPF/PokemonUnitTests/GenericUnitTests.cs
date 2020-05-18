121366ba6c6c4827c2465f511a90e4f42ef6000c
Bryant <bryantsuiskensmast@gmail.com>
2020-05-18 16:06:31 +0200

    begin unittests

---
+++ PokemonWPF/PokemonUnitTests/GenericUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokemonDAL;
+using System.Collections;
+using System.Collections.Generic;
+using PokemonWPF;
+
+namespace PokemonUnitTests
+{
+    [TestClass]
+    public class DatabaseOperationUnitTests
+    {
+        [TestMethod]
+        public void TestPartySize()
+        {
+   
+
+            //arrange
+            List<PokemonGroup> party1 = new List<PokemonGroup>();
+            List<PokemonGroup> party2 = new List<PokemonGroup>();
+
+           
+            //act
+            party1 = DatabaseOperations.SelectParty(1);
+            party2 = DatabaseOperations.SelectParty(2);
+
+            Assert.IsTrue(party1.Count < 7 && party2.Count < 7);
+
+        }
+    }
+}
