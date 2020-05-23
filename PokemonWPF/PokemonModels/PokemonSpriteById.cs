f073cc31ca6e277a67a7d85065b1dbf2862e36df
Bryant Suiskens <61180866+colerss@users.noreply.github.com>
2020-05-23 16:57:57 +0200

    SpriteByID programmed

---
+++ PokemonWPF/PokemonModels/PokemonSpriteById.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+
+namespace PokemonModels
+{
+   public class PokemonSpriteById : PokemonSprite
+    {
+
+        public PokemonSpriteById(int id) : base()
+        {
+            int baseWidth = 56;
+            int xPos = (baseWidth * id) % 896;
+            int yPos = ((int)Math.Floor((baseWidth * id) / 896.0 ) * baseWidth * 2);
+            target = new Int32Rect(xPos, yPos, target.Width, target.Height);
+        }
+    }
+}
